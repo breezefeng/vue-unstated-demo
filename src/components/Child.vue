c848d7ee1cbdfe15e87db92e37db88895351cc2a
breezefeng <fengyuqing@bbktel.com>
2020-02-01 22:38:45 +0800

    file init

---
+++ src/components/Child.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Child</h1>
+    <div>
+      <p>{{ state.count }}</p>
+      <button @click="increment">++</button>
+    </div>
+  </div>
+</template>
+<script>
+import counterContainer from '../use/counter'
+export default {
+  setup() {
+    const { state, increment } = counterContainer.useContainer()
+
+    return {
+      state,
+      increment
+    }
+  }
+}
+</script>
---
+++ src/components/Parent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Parent</h1>
+    <div>
+      <p>{{ state.count }}</p>
+      <button @click="increment">++</button>
+    </div>
+    <Child />
+  </div>
+</template>
+<script>
+import counterContainer from '../use/counter'
+import Child from './Child.vue'
+
+export default {
+  components: {
+    Child
+  },
+  setup() {
+    const { state, increment } = counterContainer.provide()
+
+    return {
+      state,
+      increment
+    }
+  }
+}
+</script>
